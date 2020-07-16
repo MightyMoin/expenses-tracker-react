8187ad9e8e7a1e2612f7d7074f32e28163f468ac
MightyMoin <lcs2019006@iiitl.ac.in>
2020-07-16 21:23:53 +0530

    Updates

---
+++ precache-manifest.16cf7b2d6f2f0651f62b1653345b9234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da77c0cef015acf094a5836bde10207a",
+    "url": "/expenses-tracker-react/index.html"
+  },
+  {
+    "revision": "c4ea959cf78a69214d9b",
+    "url": "/expenses-tracker-react/static/css/main.88551434.chunk.css"
+  },
+  {
+    "revision": "d30e2f30d2cbc55a9baa",
+    "url": "/expenses-tracker-react/static/js/2.a7d0bc73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expenses-tracker-react/static/js/2.a7d0bc73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ea959cf78a69214d9b",
+    "url": "/expenses-tracker-react/static/js/main.2f2048a3.chunk.js"
+  },
+  {
+    "revision": "d8d329328f5de7032774",
+    "url": "/expenses-tracker-react/static/js/runtime-main.b8ca3b95.js"
+  }
+]);
